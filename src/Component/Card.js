ab6cac0384c8dc38dfe6173f863a9b7d3ff0d75f
Shreya-Naithani <102851285+Shreya-Naithani@users.noreply.github.com>
2024-02-23 22:34:27 +0530

    Update Card.js

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Card = ({image,name,job}) => {
   return (
     <div className="bg-white p-4 rounded shadow-md flex flex-col items-center hover:scale-105">
-      <div className="w-full h-32 mb-4 overflow-hidden rounded-t-md">
+       <div className="flex w-full items-center justify-center object-cover">
       {image && (
           <img
             src={image}
             alt={name}
-            className="w-full h-full object-scale-down"
+          className="w-[15rem] h-48 object-cover"
             style={{ maxHeight: '100%' }}
           />
         )}
